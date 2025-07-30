02465d096c2c780988f6b3e1dc16aaa1e6735dd4
barafagnus <barafagnus@gmail.com>
2025-07-30 11:31:24 +0700

    KS-1-1 solution

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val purchaseMessage: String = "Благодарим за покупку"
+    println("Количество заказов $numberOfOrders")
+    println(purchaseMessage)
+}
