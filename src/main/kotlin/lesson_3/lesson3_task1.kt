ec042f233112d2d8f5a87270c015ab83f905ec4a
barafagnus <barafagnus@gmail.com>
2025-07-30 14:32:11 +0700

    KS-3-1 solution

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val name: String = "Vasya Pumpkin"
+    var dailyMessage: String = "Good morning"
+    println("$dailyMessage, $name")
+
+    dailyMessage = "Good evening"
+    println("$dailyMessage, $name")
+}
