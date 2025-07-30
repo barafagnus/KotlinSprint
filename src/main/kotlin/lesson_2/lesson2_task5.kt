e34dc2d1b0565ff3a6440ffe7c86d607e15eb670
barafagnus <barafagnus@gmail.com>
2025-07-30 14:19:28 +0700

    KS-2-5 solution

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val presentValue: Int = 70_000
+    val interestRate: Float = 16.7f
+    val numberOfInterestApplied: Short = 1
+    val timePeriods: Short = 20
+
+    val result: Float = presentValue * (1 + (interestRate/100) / numberOfInterestApplied).pow(numberOfInterestApplied * timePeriods)
+    println(String.format("%.3f", result))
+    // У меня получилось 1536439,875
+}
