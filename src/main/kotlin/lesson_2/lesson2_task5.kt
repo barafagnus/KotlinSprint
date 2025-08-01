c0b370e5cbe6cb4c6d190043a93f553c703ea3ce
barafagnus <barafagnus@gmail.com>
2025-08-01 14:39:19 +0700

    KS-2-5 corrected

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -3,11 +3,11 @@ package org.example.lesson_2
 import kotlin.math.pow
 
 fun main() {
-    val presentValue: Int = 70_000
-    val interestRate: Float = 16.7f
-    val numberOfInterestApplied: Short = 1
-    val timePeriods: Short = 20
+    val presentValue = 70_000
+    val interestRate = 16.7
+    val numberOfInterestApplied = 1
+    val timePeriods = 20
 
-    val result: Float = presentValue * (1 + (interestRate/100) / numberOfInterestApplied).pow(numberOfInterestApplied * timePeriods)
+    val result = presentValue * (1 + interestRate/100 / numberOfInterestApplied).pow(numberOfInterestApplied * timePeriods)
     println(String.format("%.3f", result))
 }
