cb60cfe94915adf03b51ebce2dc774a7e2ef6fdf
barafagnus <barafagnus@gmail.com>
2025-08-01 18:38:44 +0700

    KS-4-2 refactored

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,16 +1,19 @@
 package org.example.lesson_4
 
-fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
+fun main() {
     val weight1 = 20
     val volume1 = 80
 
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 соответствует категории 'Average': ${weight1 > minWeight && weight1 <= maxWeight && volume1 < maxVolume}")
-    println("Груз с весом $weight2 кг и объемом $volume2 соответствует категории 'Average': ${weight2 > minWeight && weight2 <= maxWeight && volume2 < maxVolume}")
+    val firstCargoIsAverage = weight1 > MIN_WEIGHT && weight1 <= MAX_WEIGHT && volume1 < MAX_VOLUME
+    val secondCargoIsAverage = weight2 > MIN_WEIGHT && weight2 <= MAX_WEIGHT && volume2 < MAX_VOLUME
+
+    println("Груз с весом $weight1 кг и объемом $volume1 соответствует категории 'Average': $firstCargoIsAverage")
+    println("Груз с весом $weight2 кг и объемом $volume2 соответствует категории 'Average': $secondCargoIsAverage")
 }
