b34bbe7ed6d954dc96a1fdc2d3928dd8d6f2c0b2
barafagnus <barafagnus@gmail.com>
2025-10-14 01:34:41 +0700

    KS-8-5 restyling

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,6 +1,7 @@
 fun main() {
     print("Количество планируемых ингредиентов: ")
     val arraySize = readln().toInt()
+
     if (arraySize > 0) {
         val ingredients = arrayOfNulls<String>(arraySize)
 
@@ -9,6 +10,7 @@ fun main() {
             val ingredient = readln()
             ingredients[i] = ingredient
         }
+
         println(ingredients.joinToString(separator = ", "))
     }
 }
