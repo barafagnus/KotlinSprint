9ffc547e2a525ce708cde2381cc436f703b57ad5
barafagnus <barafagnus@gmail.com>
2025-10-14 01:39:59 +0700

    KS-9-2 restyling

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -1,9 +1,11 @@
 fun main() {
     val recipe = mutableListOf("яйцо", "молоко", "соль")
     println("В рецепте есть базовые ингредиенты: $recipe")
+
     while (true) {
         print("Желаете добавить еще? >> ")
         val answer = readln()
+
         if (answer.equals("да", ignoreCase = true)) {
             print("Какой ингредиент вы хотите добавить? >> ")
             val ingredient = readln().lowercase()
