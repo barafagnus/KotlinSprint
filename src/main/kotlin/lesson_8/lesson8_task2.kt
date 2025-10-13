a40f745122015b40aa69400d70de8669740a515b
barafagnus <barafagnus@gmail.com>
2025-10-13 20:33:08 +0700

    KS-8-2 homework

---
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val borschRecipe = arrayOf("говядина", "лук", "морковь", "свекла", "картофель")
+    print("Поиск ингредиента: ")
+    val desiredIngredient = readln().lowercase()
+    for (ingredient in borschRecipe) {
+        if (desiredIngredient == ingredient) {
+            println("Ингредиент $desiredIngredient в рецепте есть")
+        } else println("Такого ингредиента в рецепте нет")
+    }
+}
