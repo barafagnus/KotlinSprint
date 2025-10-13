92b2eee6d0d6a3583b1568a9368889411e9cd953
barafagnus <barafagnus@gmail.com>
2025-10-14 01:30:47 +0700

    KS-7-3 homework resolve

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -2,7 +2,7 @@ fun main() {
     print("Введите число: ")
     val number = readln().toInt()
 
-    for (num in (0 .. number step 2)) {
+    for (num in 0..number step 2) {
         print("$num ")
     }
 }
