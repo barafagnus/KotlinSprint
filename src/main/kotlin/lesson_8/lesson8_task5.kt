fd53eea8a7b450efb4973133f210a2aa21c6c1ec
barafagnus <barafagnus@gmail.com>
2025-10-13 21:02:46 +0700

    KS-8-5 homework

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,14 @@
+fun main() {
+    print("Количество планируемых ингредиентов: ")
+    val arraySize = readln().toInt()
+    if (arraySize > 0) {
+        val ingredients = arrayOfNulls<String>(arraySize)
+
+        for (i in 0 until arraySize) {
+            print("Что добавим: ")
+            val ingredient = readln()
+            ingredients[i] = ingredient
+        }
+        println(ingredients.joinToString(separator = ", "))
+    }
+}
