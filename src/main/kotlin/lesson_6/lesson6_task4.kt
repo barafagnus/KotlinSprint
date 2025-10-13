764f543b48e33008c8363755e8f5f6d337ba4586
barafagnus <barafagnus@gmail.com>
2025-10-13 20:05:56 +0700

    KS-6-4 homework resolve

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,19 +1,16 @@
-import kotlin.random.Random
-
-
 fun main() {
-    var attempts = 3
-    val number = Random.nextInt(1, 5)
-    while (attempts > 0) {
-        print("Введите число: ")
+    var attempts = 5
+    val number = (1..9).random()
+    do {
+        print("${number} Введите число: ")
         val choice = readln().toInt()
         if (choice == number) {
             println("Это была великолепная игра!")
             break
         } else {
-            attempts -= 1
+            --attempts
             println("Неверно. Попыток осталось $attempts")
         }
-    }
+    } while (attempts > 0)
     print("Было загадано число $number")
 }
