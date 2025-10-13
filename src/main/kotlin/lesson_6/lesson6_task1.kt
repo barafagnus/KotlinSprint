1e35aac4fc591125804ca2d8050bd00afc2626c9
barafagnus <barafagnus@gmail.com>
2025-10-14 01:18:54 +0700

    KS-6-1 homework resolve

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -3,11 +3,12 @@ fun main() {
     val name = readln()
     print("Пароль: ")
     val password = readln()
+
     do {
         print("Введите логин: ")
         val nameInput = readln()
         print("Введите пароль: ")
         val passwordInput = readln()
-    } while (nameInput != name && passwordInput != password)
+    } while (nameInput != name || passwordInput != password)
     println("Авторизация прошла успешна")
 }
