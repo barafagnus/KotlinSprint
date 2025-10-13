08872938d819860ff0923519597d1f5cd32bd484
barafagnus <barafagnus@gmail.com>
2025-10-14 01:25:13 +0700

    KS-7-1 homework resolve

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,13 +1,16 @@
 const val PASSWORD_LENGTH = 6
 
 fun main() {
-
+    val numbersRange = '1'..'9'
+    val symbolsRange = 'a'..'z'
     var password = ""
-    for (char in 1..PASSWORD_LENGTH / 2) {
-        val randomNumber = ('1'..'9').random()
-        val randomSymbol = ('a'..'z').random()
+
+    for (i in 1..PASSWORD_LENGTH / 2) {
+        val randomNumber = numbersRange.random()
+        val randomSymbol = symbolsRange.random()
         password += randomNumber
         password += randomSymbol
     }
+
     print(password)
 }
