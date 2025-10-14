2bfaf679d260bf473a926fe6ca8d864056524cf2
barafagnus <barafagnus@gmail.com>
2025-10-14 15:45:48 +0700

    KS-7-5 change characters range

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,11 +1,13 @@
 const val MIN_PASSWORD_LENGTH = 6
+val CHARS = ('0'..'9') + ('a'..'z') + ('A'..'Z')
 
 fun main() {
     print("Введите длину пароля (минимум $MIN_PASSWORD_LENGTH) символов: ")
     val passwordLength = readln().toInt()
+
     if (passwordLength >= MIN_PASSWORD_LENGTH) {
-        for (char in (0 until passwordLength)) {
-            val password = ('0'..'z').random()
+        for (i in 0 until passwordLength) {
+            val password = CHARS.random()
             print(password)
         }
     } else println("Минимальная длина пароля должна составлять $MIN_PASSWORD_LENGTH символов")
