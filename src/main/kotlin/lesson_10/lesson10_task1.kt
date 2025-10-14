1c3864c5ad3cb160acda575dfd757bc82cc0b8a7
barafagnus <barafagnus@gmail.com>
2025-10-14 16:41:57 +0700

    KS-10-1 homework

---
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -0,0 +1,15 @@
+val NUMS = (1..6)
+
+fun main() {
+    val player = rollTheDice()
+    val computer = rollTheDice()
+    when {
+        player > computer -> println("Победило человечество")
+        computer > player -> println("Победила машина")
+        else -> println("Победила дружба")
+    }
+}
+
+fun rollTheDice(): Int {
+    return NUMS.random()
+}
