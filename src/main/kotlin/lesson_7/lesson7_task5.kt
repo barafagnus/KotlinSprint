6d6d1d765d941e1c83b1bea40945dbae7b5da9eb
barafagnus <barafagnus@gmail.com>
2025-10-15 21:38:39 +0700

    KS-7-5 resolve 2

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,21 +1,22 @@
 const val MIN_PASSWORD_LENGTH = 6
 
 fun main() {
-    val DIGITS = '0'..'9'
-    val LOWERCASE = 'a'..'z'
-    val UPPERCASE = 'A'..'Z'
+    val digits = '0'..'9'
+    val lowerCase = 'a'..'z'
+    val upperCase = 'A'..'Z'
+    val symbolsRange = digits + lowerCase + upperCase
 
     print("Введите длину пароля (минимум $MIN_PASSWORD_LENGTH) символов: ")
     val passwordLength = readln().toInt()
     val password = mutableListOf<Char>()
 
     if (passwordLength >= MIN_PASSWORD_LENGTH) {
-        password.add(DIGITS.random())
-        password.add(LOWERCASE.random())
-        password.add(UPPERCASE.random())
+        password.add(digits.random())
+        password.add(lowerCase.random())
+        password.add(upperCase.random())
 
         for (i in 3 until passwordLength) {
-            password.add((DIGITS + LOWERCASE + UPPERCASE).random())
+            password.add((symbolsRange).random())
         }
         val resultPassword = password.shuffled().joinToString(separator = "")
         println(resultPassword)
