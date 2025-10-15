40f9ac758d855cecccbcc800ce3f341cb45d0122
barafagnus <barafagnus@gmail.com>
2025-10-15 13:50:06 +0700

    KS-7-4 resolve

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -2,9 +2,9 @@ fun main() {
     print("Сколько секунд засечь: ")
     val seconds = readln().toInt()
 
-    for (second in (seconds downTo 1)) {
-        println("Осталось $second секунд")
+    for (second in seconds downTo 1) {
         Thread.sleep(1000)
+        println("Осталось $second секунд")
     }
     println("Время вышло")
 }
