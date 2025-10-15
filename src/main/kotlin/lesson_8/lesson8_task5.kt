c83ecb1114391e8518ba3c0c7f6db5277dc377cd
barafagnus <barafagnus@gmail.com>
2025-10-15 14:29:31 +0700

    KS-8-5 resolve

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,16 +1,14 @@
 fun main() {
     print("Количество планируемых ингредиентов: ")
-    val arraySize = readln().toInt()
+    val amount = readln().toInt()
 
-    if (arraySize > 0) {
-        val ingredients = arrayOfNulls<String>(arraySize)
-
-        for (i in 0 until arraySize) {
-            print("Что добавим: ")
-            val ingredient = readln()
-            ingredients[i] = ingredient
+    val ingredients = buildString {
+        repeat(amount) { index ->
+            print("Добавить: ")
+            append(readln())
+            if (index < amount - 1) append(", ")
         }
-
-        println(ingredients.joinToString(separator = ", "))
     }
+
+    println(ingredients)
 }
