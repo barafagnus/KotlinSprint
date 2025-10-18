1ee188a2c5fbe1bc2be30f6540f98e3de0ce75ec
barafagnus <barafagnus@gmail.com>
2025-10-18 21:54:26 +0700

    KS-10-5 homework

---
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -0,0 +1,56 @@
+fun main() {
+    print("Логин: ")
+    val login = readln()
+
+    print("Пароль: ")
+    val password = readln()
+
+    val token = getToken(login, password)
+    if (token != null) {
+        val shoppingCart = getShoppingCart(token)
+        shoppingCart?.forEach { println(it) }
+    } else println("Ошибка авторизации")
+}
+
+
+data class User(val login: String, val password: String)
+
+val users = listOf<User>(
+    User(login = "usr1", password = "1234"),
+    User(login = "usr2", password = "4321")
+)
+
+var credentials: User? = null
+const val TOKEN_LENGTH = 32
+var generatedTokens = mutableListOf<String>()
+
+fun getToken(authLogin: String, authPassword: String): String? {
+    val digits = '0'..'9'
+    val lowerCase = 'a'..'z'
+    val upperCase = 'A'..'Z'
+    val allowedChars = digits + lowerCase + upperCase
+
+    if (users.any { it.login == authLogin && it.password == authPassword }) {
+        credentials = User(login = authLogin, password = authPassword)
+        val token = buildString {
+            repeat(TOKEN_LENGTH) {
+                append(allowedChars.random())
+            }
+        }
+        generatedTokens.add(token)
+        return token
+    } else return null
+}
+
+
+data class Item(val title: String, val category: String, val price: Double)
+
+val items = listOf<Item>(
+    Item(title = "Игровая мышь Rat v1.0", category = "Периферия для компьютеров", price = 1200.0),
+    Item(title = "Монитор Eyes 24.5", category = "Мониторы", price = 20000.0),
+    Item(title = "Клавиатура Keyz", category = "Периферия для компьютеров", price = 2300.0)
+)
+
+fun getShoppingCart(token: String?): List<Item>? {
+    return if (token in generatedTokens) items else null
+}
