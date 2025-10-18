87212b9e4e082aa283064bf03f1bea282634b5b9
barafagnus <barafagnus@gmail.com>
2025-10-19 00:38:27 +0700

    KS-11-2 homework

---
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -0,0 +1,40 @@
+package lesson_11
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val mail: String,
+    var bio: String? = null,
+) {
+    fun getInfo() {
+        println("$id $login $password $mail $bio")
+    }
+
+    fun setBio() {
+        print("Опишите биографию: ")
+        bio = readln()
+    }
+
+    fun changePassword(currentPassword: String) {
+        if (currentPassword == password) {
+            print("Новый пароль: ")
+            password = readln()
+            println("Пароль изменен")
+        } else println("Неправильный пароль")
+    }
+}
+
+
+fun main() {
+    val user = User2(
+        id = 1,
+        login = "User1",
+        password = "1234",
+        mail = "user1@gmail.com"
+    )
+
+    user.setBio()
+    user.changePassword(user.password)
+    user.getInfo()
+}
