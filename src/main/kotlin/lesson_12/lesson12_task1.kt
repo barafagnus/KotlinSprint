a8e80fe989b015b32885cfda2e9f5a248f6792de
barafagnus <barafagnus@gmail.com>
2025-10-19 20:53:08 +0700

    KS-12-1 homework

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,33 @@
+class DailyWeather() {
+    var daytimeTemperature: Double = 20.0
+    var nightTemperature: Double = 10.0
+    var precipitation: Boolean = false
+
+    fun showWeather() {
+        println("Температура днем: $daytimeTemperature")
+        println("Температура ночью: $nightTemperature")
+        println("Осадки: ${if (precipitation) "да" else "нет"}")
+    }
+}
+
+fun main() {
+
+    val mondayTemperature = DailyWeather()
+    val tuesdayTemperature = DailyWeather()
+    mondayTemperature.showWeather()
+    println()
+    tuesdayTemperature.showWeather()
+    println()
+
+    mondayTemperature.daytimeTemperature = 25.0
+    mondayTemperature.nightTemperature = 8.0
+    mondayTemperature.precipitation = true
+
+    tuesdayTemperature.daytimeTemperature = 23.0
+    tuesdayTemperature.nightTemperature = 11.0
+
+    mondayTemperature.showWeather()
+    println()
+    tuesdayTemperature.showWeather()
+}
+
