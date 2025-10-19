c53b5ded3909106e5ee21f1415c7e7151af885ea
barafagnus <barafagnus@gmail.com>
2025-10-19 23:08:14 +0700

    KS-12-4 homework

---
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -0,0 +1,21 @@
+class WeatherForecast(
+    _daytimeTemperature: Int,
+    _nightTemperature: Int,
+    _precipitation: Boolean
+) {
+    val daytimeTemperature = _daytimeTemperature - 273
+    val nightTemperature = _nightTemperature - 273
+    val precipitation = _precipitation
+
+    init {
+        println("t днем: $daytimeTemperature, ночью $nightTemperature, осадки: ${if (precipitation) "да" else "нет"}")
+    }
+
+}
+
+fun main() {
+
+    val mondayWeather = WeatherForecast(300, 310, true)
+
+}
+
