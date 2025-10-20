920835b851f2113fd23d4ee68451f088d39385ef
barafagnus <barafagnus@gmail.com>
2025-10-20 15:58:58 +0700

    KS-13-5 homework

---
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -0,0 +1,34 @@
+package lesson_13
+
+class PhContact(
+    val name: String,
+    val phone: Long?,
+    val company: String? = null
+) {
+    fun showUserInfo() {
+        println("- Имя: $name\n- Телефон: $phone\n- Компания: ${company ?: "не указано"}")
+    }
+}
+
+fun main() {
+    val contacts = listOf<PhContact>(
+        PhContact("Alex", 79000000000),
+        PhContact("Steve", 79000000001),
+        PhContact("Kate", 79000000002, "null"),
+        PhContact("Serhio", 79000000003, "Reddit"),
+        PhContact("Bob", 79000000004, "Google"),
+    )
+
+    val phoneNumber = try {
+        print("Введите номер: ")
+        readLine()?.toLong()
+
+    } catch (e: NumberFormatException) {
+        println(e)
+        print("Введите номер: ")
+        readLine()?.filter { it.isDigit() }?.toLongOrNull()
+    }
+
+    contacts.find { it.phone == phoneNumber }?.showUserInfo()
+
+}
