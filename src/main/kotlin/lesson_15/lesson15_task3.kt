a4d9e14996f47e5bd226ff721b0021eedef64c84
barafagnus <barafagnus@gmail.com>
2025-10-23 19:58:20 +0700

    KS-15-3 homework

---
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -0,0 +1,55 @@
+abstract class ForumMember {
+
+    abstract val name: String
+
+    companion object {
+        private var counter = 1
+    }
+
+    val memberId: Int = counter++
+
+    fun readForum() {
+        println("$name читает форум")
+    }
+
+    fun sendMessage(text: String) {
+        println("$name: $text")
+    }
+
+}
+
+class FUser(
+    override val name: String
+) : ForumMember() {
+
+}
+
+class FAdmin(
+    override val name: String
+) : ForumMember() {
+
+    fun deleteMessage(messageId: Int) {
+        println("$name удалил сообщение $messageId")
+
+    }
+
+    fun deleteUser(memberId: Int) {
+        println("$name удалил пользователя $memberId")
+    }
+}
+
+fun main() {
+
+    val user = FUser("User")
+    user.sendMessage("Hello")
+    user.readForum()
+
+    println()
+
+    val admin = FAdmin("Admin")
+    admin.sendMessage("Hi")
+    admin.readForum()
+    admin.deleteUser(user.memberId)
+    admin.deleteMessage(1)
+
+}
