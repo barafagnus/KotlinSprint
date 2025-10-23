38fa18a63e979427e2e9507209073df362796a40
barafagnus <barafagnus@gmail.com>
2025-10-23 19:05:10 +0700

    KS-15-2 homework

---
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -0,0 +1,25 @@
+abstract class WeatherStationStats
+
+class Temperature(val temperature: Double) : WeatherStationStats()
+
+class PrecipitationAmount(val precipitationAmount: Double) : WeatherStationStats()
+
+class WeatherServer() {
+    fun sendMessage(weatherStationStats: WeatherStationStats) {
+        when (weatherStationStats) {
+            is Temperature -> println("Temperature: ${weatherStationStats.temperature}")
+            is PrecipitationAmount -> println("Precipitations: ${weatherStationStats.precipitationAmount}")
+        }
+    }
+}
+
+fun main() {
+
+    val temperature = Temperature(25.0)
+    val precipitationAmount = PrecipitationAmount(4.0)
+
+    val weatherServer = WeatherServer()
+    weatherServer.sendMessage(temperature)
+    weatherServer.sendMessage(precipitationAmount)
+
+}
