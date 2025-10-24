1b7d26f934d82f3da90d103fd66d45bbbc8b242e
barafagnus <barafagnus@gmail.com>
2025-10-24 19:25:03 +0700

    KS-14-1 resolve

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,19 +1,19 @@
 package lesson_14
 
 open class Liner(
-    val speed: Int = 10,
-    val capacity: Int = 20
+    open val speed: Int = 10,
+    open val capacity: Int = 20
 )
 
 class IceBreaker(
-    speed: Int = 5,
-    capacity: Int = 10,
+    override val speed: Int = 5,
+    override val capacity: Int = 10,
     val canChopIce: Boolean = true
 ) : Liner(speed, capacity)
 
 class Cargo(
-    speed: Int = 5,
-    capacity: Int = 40
+    override val speed: Int = 5,
+    override val capacity: Int = 40
 ) : Liner(speed, capacity)
 
 fun main() {
